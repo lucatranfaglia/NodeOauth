1c4de3b34a3309d8e73818659aadf34971f291d6
Luca Tranfaglia <lucatranfaglia@Lucky.local>
2023-06-18 19:33:01 +0200

    feat: jwt (sign, verify)

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
   port:3000,
   dbUri: "mongodb://localhost:27017/user.api.tutorial",
   logLevel: "info",
+  accessTokenPrivateKey: '',
+  refreshTokenProvateKey: '',
   smtp:{
     user: "123",
     pass: "asdqwe",
---
+++ src/utils/jwt.ts
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken'
+import config from 'config'
+
+
+/**
+ * 
+ * @param object 
+ * @param keyName 
+ * @param options 
+ * @returns 
+ */
+export function signJwt(
+  object: Object, 
+  keyName: 'accessTokenPrivateKey' | 'refreshTokenPrivate',
+  options?: jwt.SignOptions | undefined
+){
+  const signingKey = Buffer.from(config.get<string>(keyName), "base64").toString("ascii");
+
+  return jwt.sign(object, signingKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  })
+}
+
+/**
+ * 
+ * @param token 
+ * @param keyName 
+ * @returns 
+ */
+export function verifyJwt<T>(
+  token: string, 
+  keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
+): T | null
+{
+  const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString("ascii");
+  try {
+    const decoded = jwt.verify(token, publicKey) as T;
+    return decoded;
+  } catch (error) {
+    return null;
+  }
+}
+
+
+
+
+export default signJwt
